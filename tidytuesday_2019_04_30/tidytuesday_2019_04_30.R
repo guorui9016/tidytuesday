f61536cb1790afb8dbd0ed40b20cb6563958ae0a
Ray <guorui9016@gmail.com>
2019-05-03 22:51:49 +1000

    update 3 plots for bird in Chicago.

--- tidytuesday_2019_04_30/tidytuesday_2019_04_30.R
+++ tidytuesday_2019_04_30/tidytuesday_2019_04_30.R
@@ -5,7 +5,13 @@ mp_light <- read_csv("tidytuesday_2019_04_30/data/mp_light.csv")
 bird_collisions <- read_csv("tidytuesday_2019_04_30/data/bird_collisions.csv" )
 bird_call <- read_table2("tidytuesday_2019_04_30/data/bird_call.csv")
 
-# In Q1, I learn how to use month function transfer number to words.
+####################################################################
+
+# 1. Learn how to use month function transfer number to words.
+# 2. fct_reorder and reorder function.
+
+
+
 
 # Question 1: Status of bird collision data
 
@@ -23,11 +29,39 @@ bird_stat %>%
   labs(x=" Month",
        y = "Number of collisions",
        title = "Does bird happy now?")+
-  facet_wrap(~locality)
+  facet_wrap(~locality)+
+  theme_classic()
+  
 
-# Question2: From the Q1 I just want know which kind of bird become to more?
+# Question 2: From the Q1 I just want know which kind of bird become to more?
 
+q2 <- bird_stat %>% 
+        count(family, year, locality) %>% 
+        mutate(family = fct_reorder(family, -n)) %>%
+        filter(n>30)
 
+#### way 1
 
+q2 %>% 
+  ggplot(aes(year, n, fill = family))+
+  geom_area(position = "identity", alpha = 0.75)+
+  facet_wrap(~locality)+
+  labs(x=" Year",
+       y = "Number of collisions",
+       title = "Which kind of bird come to Chicago?")+
+  theme_classic()
+        
+#### way 2
 
+q2 %>% 
+  ggplot(aes(year, y = reorder(family, n)))+
+  geom_point(aes(size = n), alpha = 0.5)+
+  facet_wrap(locality~.)+
+  labs(x=" Year",
+       y = "Family",
+       title = "Which kind of bird come to Chicago?",
+       size = "Number of collisions")+
+  theme_classic()
+  
+# Question3:
 
