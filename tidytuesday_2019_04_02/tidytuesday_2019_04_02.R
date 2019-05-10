405c96956ccdb1def6b02b673fa31acb929eae7d
guorui9016 <guorui9016@gmail.com>
2019-05-10 16:07:33 +1000

    change method to upload data

--- tidytuesday_2019_04_02/tidytuesday_2019_04_02.R
+++ tidytuesday_2019_04_02/tidytuesday_2019_04_02.R
@@ -2,25 +2,24 @@
 
 
 #### 2. Split string by " " in data frame ??
+Sys.setlocale("LC_TIME", "English")
 
 library(tidyverse)
 library(lubridate)
 
-bike_raw <- read_csv("tidytuesday_2019_04_02/data/bike_traffic.csv")
-  
-bike <- bike_raw %>% 
-  mutate(date = dmy_hms(date)) %>% 
-  mutate(weekday = wday(date, label = T), 
-         year = year(date)) %>% 
-  filter(weekday != "NA")
+bike <- read_csv("tidytuesday_2019_04_02/data/bike_traffic.csv", col_types = "cccdd") %>% 
+  mutate(date = mdy_hms(date)) %>% 
+  mutate(day = wday(date, label = T),
+         year = year(date))
+
 
 # Question 1: Which day is a busy day?
 
 bike %>% 
-  group_by(weekday, year) %>% 
+  group_by(day, year) %>% 
   filter(year !=2019 ) %>% 
   summarise(sum = sum(bike_count, na.rm = T)) %>% 
-  ggplot(aes(x= weekday, y = sum, group = year, color= as.factor(year)))+
+  ggplot(aes(x= day, y = sum, group = year, color= as.factor(year)))+
   geom_line(size = 2, alpha = 0.5)+
   geom_point(shape = 20, size  = 5,alpha = 0.5)+
   labs(x="Week", y = "Number of bikes", title = "Do people like ride bike now?", color = "Year")+
@@ -28,16 +27,24 @@ bike %>%
   
 # Question 2: Busy road?
 
-q2 <- bike %>% 
-  group_by(weekday, crossing) %>% 
+bike %>% 
+  group_by(day, crossing) %>% 
   summarise(sum = sum(bike_count, na.rm = T)) %>% 
-  ggplot(aes(x=weekday, y=sum, group = crossing, color = crossing))+
+  ggplot(aes(x=day, y=sum, group = crossing, color = crossing))+
   geom_line(size =2 , alpha = 0.5)+
   geom_point(shape = 20, size  = 5,alpha = 0.5)+
   labs(x="Week", y = "Number of bikes", title = "How busy of road", color = "Road")+
   theme_classic()
 
-q2
-  
 
-# Question 3: 
+# Question 3: I want see what diff between weekday and weekend by diff road.
+
+q3 <- bike %>% 
+  mutate(day_type = if_else(day %in% c("Sat", "Sun"), "weekdend", "weekday")) %>% 
+  group_by(day, day_type, crossing) %>% 
+  summarise(sum = sum(bike_count, na.rm = T)) %>% 
+  ggplot(aes(x =crossing , y =sum ,  fil= day_type))+
+  geom_bar(stat = "identity", alpha = 0.6)
+
+
+q3
