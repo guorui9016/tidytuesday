7b8adb6d7698a91c1fac9213ac1e2ee3fd2f5502
guorui9016 <guorui9016@gmail.com>
2019-05-12 00:18:10 +1000

    add one more plot for bike data and update pet data.

---
+++ tidytuesday_2019_03_26/tidytuesday_2019_03_26.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+pets <- read_csv("tidytuesday_2019_03_26/data/seattle_pets.csv")
+
+
+# Question 1: Cat VS Dog (饼图)
+
+# Question 2: Top 10 name of cat and dog
+
+# Question 3: Cat city or dog city?
+
+# Question 4: Pet number growing?
+
+# Question 5: Which name is good for both pet?
+
