c03b42f910037f05e8c8c84c88e060cf8f3fbbbc
guorui9016 <guorui9016@gmail.com>
2019-05-16 16:55:22 +1000

    3 plot update

--- tidytuesday_2019_05_14/tidytuesday_2019_05_14.R
+++ tidytuesday_2019_05_14/tidytuesday_2019_05_14.R
@@ -7,3 +7,40 @@ nobel_winner_all_pubs <- read_csv("tidytuesday_2019_05_14/data/nobel_winner_all_
 nobel_winner <- read_csv("tidytuesday_2019_05_14/data/nobel_winners.csv")
 physics <- read_csv("tidytuesday_2019_05_14/data/Physics publication record.csv")
 prize <- read_csv("tidytuesday_2019_05_14/data/Prize-winning paper record.csv")
+
+
+# Queston 1: Still man's world?
+
+nobel_winner %>% 
+  filter(gender!="NA") %>% 
+  ggplot(aes(x=category,fill=gender))+
+  geom_bar(position = "dodge")+
+  coord_flip()+
+  theme_light()+
+  labs(x="", y= "Number of Nobel Laureates", title = "Still man's world?")
+
+# Question 2: Following Q1, I want know should world been changed recently?
+
+nobel_winner %>% 
+  filter(gender!="NA") %>% 
+  count(category, gender, prize_year) %>% 
+  mutate(decade = (prize_year %/% 10) * 10 ) %>% 
+  ggplot(aes(x=decade, y=n, fill=gender))+
+  geom_bar(stat = "identity", position = "dodge")+
+  coord_flip()+
+  facet_wrap(category~.)+
+  theme_light()+
+  labs(x="decade", y="Number of Nobel Laureates", fill="Gender",
+     title = "Still man's world?")
+  
+# Question 3: Which country is the winer?
+
+nobel_winner %>% 
+  count(organization_country) %>% 
+  filter(organization_country!="NA",
+         n > 2) %>% 
+  ggplot(aes(x=reorder(organization_country, n), y = n ))+
+  geom_col()+
+  coord_flip()
+
+# Question 4: Age when get the prize.
